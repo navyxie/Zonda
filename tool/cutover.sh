7276b9904fa2b50295d113f8b0a705db0b6e006d
Degas <smallsmallwolf@gmail.com>
2012-08-29 13:48:33 +0800

    update

--- tool/cutover.sh
+++ tool/cutover.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# build 选项：根据package.json打包src下的代码
+# lib选项：还可跟一个参数表示要打包具体的某个第三方模块，不加选项，则打包全部的第三方模块
 
 case $1 in
     build)
@@ -8,7 +10,7 @@ case $1 in
     lib)
         cd ../
         #spm build --convertStyle=default --dist=./lib --src=./lib-debug
-        spm build --convertStyle=default --dist=./lib --src=./lib-debug
+        spm build --convertStyle=default --dist=./lib/$2 --src=./lib-debug/$2
     ;;
     *)
         echo $1 ? 表示没有这个选项 =。=
