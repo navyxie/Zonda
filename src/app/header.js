4cf90238a234dba479c709f7122c779b4c0b650e
Degas <smallsmallwolf@gmail.com>
2012-08-29 12:35:05 +0800

    增加demo

--- src/app/header.js
+++ src/app/header.js
@@ -3,5 +3,14 @@
 // 头部
 
 define(function ( require, exports, module) {
-    console.log('header');
+    // test jquery-ui
+    var $ = require('jquery-ui');
+    $("#hh").datepicker();
+
+    // test tips
+    var Tips = require('../module/tips.module');
+    Tips.some({
+        "target" : $("#tips-p")
+    });
+
 });
