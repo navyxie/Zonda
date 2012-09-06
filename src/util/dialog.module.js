5cff3149aae915586a247c2a675d8bd36d2149c3
Degas <smallsmallwolf@gmail.com>
2012-09-06 10:44:12 +0800

    加入dialog方法

---
+++ src/util/dialog.module.js
@@ -0,0 +1,10 @@
+// dialog.module.js
+// ----------------
+// 基于Bootstrap的对话框封装
+
+define(function( require, exports, moudle ){
+    var $ = require('bootstrap');
+
+    // 准备模板
+    var dialogTPL = require('tpl/dialog.tpl');
+});
