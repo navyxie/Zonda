9c5053ef887d7a5814aa0f349dec408ee5fdd390
Degas <smallsmallwolf@gmail.com>
2012-09-11 09:26:54 +0800

    11

--- src/util/route.module.js
+++ src/util/route.module.js
@@ -5,6 +5,19 @@
 // 但是对于路由来说，加载是一个自动的的过程，需要使用变量
 // 于是使用 eval('require("' + a + '")') 实现
 // 性能会受到很大的影响，先实现，再优化，或者再废弃这种方式
+//
+// 兼容Niko的方案，采用回调函数的形式执行
+// 代码示例如下:
+/**
+Util.route({
+	'#index' : function(){
+		require.async("../app/index/");
+	},
+	'#header' : function(){
+		require.async("../app/header/");
+	}
+});
+*/
 
 define(function ( require, exports, module) {
     return function ( ruler ) {
@@ -12,10 +25,15 @@ define(function ( require, exports, module) {
         var _ = require('underscore');
 
         _.each( ruler, function ( action, selector ) {
-            if ( $(selector)[0] ) {
+            // 传入callback形式
+            if ( $(selector)[0] && _.isFunction(action) ) {
+                action();
+            } else if ( $(selector)[0] && _.isString(action) ) {
+                // 传入字符串
                 eval('require.async("../app/' + action + '")');
-            }
+            } // END if
         });
 
-    };
+    }; // END return
+
 });
