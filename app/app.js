55e98b054735e7958f4a03f5eec0e69f1c9ce20a
Degas <smallsmallwolf@gmail.com>
2012-10-08 22:24:47 +0800

    close #26 CMD jquery到私有源

--- app/app.js
+++ app/app.js
@@ -3,8 +3,11 @@
  * 地阿杜
  */
 define(function(require, exports, module){
-    var Util = require("util");
+    //var Util = require("util");
+    var $ = require("jquery");
 
-    console.info(Util);
+    //console.info(Util);
+    console.info($);
+    window.$ = $;
 
 });
--- init.js
+++ init.js
@@ -1,24 +1,16 @@
-// SeaJS 入口文件
-// --------------
-// author degas.yerya
-
-// 配置seajs
 seajs.config({
-    //顶级标识
     base : '/Zonda',
 
-    // 配置别称
     alias : {
         'util' : 'util/util',
         'underscore' : 'lib/underscore/1.3.3/underscore',
         'bootstrap' : 'lib/bootstrap/2.1.0/bootstrap',
-        'jquery' : 'lib/jquery/1.8.0/jquery',
+        'jquery' : 'lib/jquery/1.8.2/jquery',
         'jquery-ui' : 'lib/jquery-ui/1.8.22/jquery-ui',
         'easing' : 'lib/easing/1.3.0/jquery.easing',
         'backbone' : 'lib/backbone/0.9.2/backbone'
     },
 
-    // 预加载
     preload : [
         'lib/modernizr/2.6.1/modernizr',
         'core/1.2.1/plugin-text',
@@ -29,7 +21,4 @@ seajs.config({
     charset : 'utf-8'
 });
 
-// 启动 DOM 路由
-// 如果使用'util-debug'模式，则会加载对应的开发版本的模块
-// 打包部署以后将这里改成'util'即可
-seajs.use('init');
+seajs.use('app/app');
