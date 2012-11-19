1bc820cf1eca37e21f190d5cbaa7927ea9a34935
Degas <smallsmallwolf@gmail.com>
2012-11-19 14:49:02 +0800

    加入SinonJS做各种Mock

--- test/test.js
+++ test/test.js
@@ -2,12 +2,22 @@
  * test
  */
 define(function(require, exports, module){
+    var _ = require('underscore');
+
+    // 依赖Sinon
+    require('sinon');
+
     // QUit样式
-    require('test/qunit/1.10.0/qunit.css');
+    // 默认样式
+    //require('test/qunit/1.10.0/qunit.css');
+    // 新主题
+    require('test/themes/ninja.css');
 
-    // test page
-    require('./case/page');
+    // 加载PHP渲染的测试样例数据
+    // TODO 后端考虑改成Nodejs的
+    var caseDate = window.php_var;
 
-    // test comment
-    require('./case/comment');
+    _.each( caseDate, function( cell ) {
+        require.async( cell.url );
+    });
 });
