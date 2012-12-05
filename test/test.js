c7d6ad5cbe0f5c20b61b8dda65c6b3239f50bc6e
Degas <smallsmallwolf@gmail.com>
2012-12-05 19:54:52 +0800

    解决冲突

--- test/test.js
+++ test/test.js
@@ -17,12 +17,9 @@ define(function(require, exports, module){
     // JSON RPC 模块
     require('./case/rpc');
 
-<<<<<<< HEAD
     // localStorageSync for Backbone
     require('./case/localStorageSync');
 
-=======
->>>>>>> f68c72ddfa38a7f7f669170e632fc626e1228a7e
     // 读取当前目录下的case.json
     /**
      * Node监听目录变化不靠谱，暂无解决办法，先手动解决
