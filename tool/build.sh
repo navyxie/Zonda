6c73b01e83fba08f25082d6d9400317313067efe
Degas <smallsmallwolf@gmail.com>
2013-03-04 11:54:29 +0800

    finish the ./build.sh dev

--- tool/build.sh
+++ tool/build.sh
@@ -17,18 +17,26 @@ case $1 in
   # make alias and plugins of SeaJs config
   # TODO do not finish yet
 
+  # build the framework-dev.js
   # Cat list. !DO NOT change the file order!!
+
   cd $seajs_dir
   cat sea-debug.js \
       plugin-text.js \
       plugin-debug.js \
-      plugin-flush.js \
       plugin-nocache.js \
       plugin-shim.js \
       plugin-warning.js \
       plugin-combo.js \
+      plugin-flush.js \
       $project_dir/etc/env.js \
       > $dist_dir/framework-dev.js
+
+  # build the dist-dev.js
+  # just load the app.js
+  echo 'seajs.use("/assets/src/app");seajs.flush();' > $dist_dir/dist-dev.js
+
+  echo "Zonda in 'DEV' status Now."
 
   ;; # END dev
 
