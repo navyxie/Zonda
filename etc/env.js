6363820d5fa2e44b30f03a6a6d47c8b18617e485
Degas <smallsmallwolf@gmail.com>
2013-03-06 22:11:46 +0800

    update etc

---
+++ etc/env.js
@@ -0,0 +1,5 @@
+seajs.config({
+  base: "/assets",
+  charset: "utf-8",
+  alias: {"underscore":"vendor/Zonda/vendor/underscore/1.4.4/underscore","jquery":"vendor/Zonda/vendor/jquery/1.9.1/jquery"}
+});
