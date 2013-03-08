af49c0ccc88dec155882933f30040dbc33b32e01
Degas <smallsmallwolf@gmail.com>
2013-03-08 09:18:42 +0800

    create test.sh

---
+++ tool/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# use npm test and vows for Zonda
+
+x=`echo $0 | grep "^/"`
+pwdp=`pwd`
+if test "${x}"; then                                                                                                                         
+  dir=`dirname $0`
+else
+  dir=`dirname $pwdp/$0`
+fi
+cd $dir
+# go to the this shell dir
+
+cd ../
+
+# install Zonda Test
+npm install
+
+npm test
+
+#rm -rf node_modules
